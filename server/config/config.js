83f4215751b486036a8c8ee498b44cf1b4bf3a21
yerson <yerson.fuentes2012@gmail.com>
2020-07-04 00:14:00 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 //puerto
 //=====================
 process.env.PORT = process.env.PORT || 3000
+
+//=====================
+//entorno
+//=====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=====================
+//base de datos
+//=====================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://yerson:QP0Gdnskt8iQueQ4@cluster0.6hewx.azure.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
